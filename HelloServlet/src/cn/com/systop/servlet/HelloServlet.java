fe8fc7d8291bd5bbb44bd1a1d8015187f12b7fd3
Lansefanggezi <lansefanggezi123@163.com>
2018-03-24 22:43:47 +0800

    HelloServlet

--- HelloServlet/src/cn/com/systop/servlet/HelloServlet.java
+++ HelloServlet/src/cn/com/systop/servlet/HelloServlet.java
@@ -1,6 +1,8 @@
 package cn.com.systop.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,6 +37,24 @@ public class HelloServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Hello Servlet");
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
 	}
 
 }
