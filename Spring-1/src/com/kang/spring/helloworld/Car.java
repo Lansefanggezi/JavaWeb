0c3671fefb05ca655fe2bd3c342d959b4b128c66
Lansefanggezi <lansefanggezi123@163.com>
2018-04-06 10:28:06 +0800

    context無法區別參數個數相同的構造器時，可以通過type屬性

--- Spring-1/src/com/kang/spring/helloworld/Car.java
+++ Spring-1/src/com/kang/spring/helloworld/Car.java
@@ -4,15 +4,23 @@ public class Car {
 
 	private String brand;
 	private String corp;
-	private int  price;
+	private double  price;
 	private int  maxSpeed;
 	
-	
-	public Car(String brand, String corp, int price) {
+	//構造器一
+	public Car(String brand, String corp, Double price) {
 		super();
 		this.brand = brand;
 		this.corp = corp;
 		this.price = price;
+	}
+
+	//構造器二 參數列表不同，類型不同
+	public Car(String brand, String corp, int maxSpeed) {
+		super();
+		this.brand = brand;
+		this.corp = corp;
+		this.maxSpeed = maxSpeed;
 	}
 
 
--- Spring-1/src/com/kang/spring/helloworld/Main.java
+++ Spring-1/src/com/kang/spring/helloworld/Main.java
@@ -8,12 +8,17 @@ public class Main {
 	public static void main(String[] args) {
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-		HelloWorld helloWorld =  (HelloWorld) context.getBean("helloworld");
+//		HelloWorld helloWorld =  (HelloWorld) context.getBean("helloworld");
+//		
+//		helloWorld.hello();
+//		
+//		Car car =  (Car) context.getBean("Car");
+//		System.out.println(car.toString());
 		
-		helloWorld.hello();
+		Car car2 = (Car) context.getBean("car2");
+		System.out.println(car2.toString());;
+		//使用構造器注入時。有可能容器無法識別不同的構造器。所以需要通過不同的參數個數或者類型來幫助容器區別
 		
-		Car car =  (Car) context.getBean("Car");
-		System.out.println(car.toString());
 	}
 
 }
