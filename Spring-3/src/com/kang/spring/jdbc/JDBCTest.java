f42c345fb6741ebc09c5a31090284dd70a13f572
Lansefanggezi <lansefanggezi123@163.com>
2018-04-07 17:53:19 +0800

    SqlParameterSource 的实现类BeanPropertySqlParameterSource

--- Spring-3/src/com/kang/spring/jdbc/JDBCTest.java
+++ Spring-3/src/com/kang/spring/jdbc/JDBCTest.java
@@ -1,8 +1,10 @@
 package com.kang.spring.jdbc;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -12,23 +14,61 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 public class JDBCTest {
 
 	private ApplicationContext context;
 	private JdbcTemplate jdbcTemplate;
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	{
 		context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 		jdbcTemplate = (JdbcTemplate) context.getBean("jdbcTemplate");
+		namedParameterJdbcTemplate = context.getBean(NamedParameterJdbcTemplate.class);
 	}
 	
+	/**
+	 * 将sql中的参数封装成对象 参数和对象中的属性名必须一致
+	 * 调用 SqlParameterSource 的实现类BeanPropertySqlParameterSource（）；
+	 * */
+	@Test
+	public void testBeanParameterJdbcTemplate()
+	{
+		String url = "insert into customers (username,userpassword,balance) values(:username,:password,:balance)";
+		
+		User user = new User();
+		user.setUsername("wangshaoming");
+		user.setPassword("wang");
+		user.setBalance(300);
+		SqlParameterSource paramSource = new BeanPropertySqlParameterSource(user);
+		namedParameterJdbcTemplate.update(url, paramSource);
+	}
+	
+	/**
+	 * 可以给参数起名字
+	 * 没有无参构造，必须给构造器赋值 DataSource
+	 * */
+	@Test
+	public void testNameParemeterJdncTemplate()
+	{
+//		String url = "insert into customers (username,userpassword,balance) values(:name,:password,:balance)";
+		String url = "update customers set balance = :balance where username = 'fandaoyan'";
+		Map<String, Object> paramMap =  new HashMap<>();
+		paramMap.put("name", "fandaoyan");
+		paramMap.put("password", "fan");
+		paramMap.put("balance", 300);
+		namedParameterJdbcTemplate.update(url, paramMap);
+		
+	}
 	/**
 	 * 获取单个列的值，或者统计查询
 	 * */
 	@Test
 	public void testQueryForObject()
 	{
-		String querySQL = "SELECT COUNT(*)  FroM  t_user ";
+		String querySQL = "SELECT COUNT(*)  FroM  customers ";
 		Long count =  jdbcTemplate.queryForObject(querySQL, long.class);
 		System.out.println(count);
 	}
@@ -39,7 +79,7 @@ public class JDBCTest {
 	@Test
 	public void testQueryForList()
 	{
-		String sql = "SELECT username username, userpassword userpassword FROM t_user ";
+		String sql = "SELECT username username, userpassword userpassword FROM customers ";
 		RowMapper<User> user = new BeanPropertyRowMapper<>(User.class);
 		
 		List<User>  listuser = jdbcTemplate.query(sql, user);
@@ -51,7 +91,7 @@ public class JDBCTest {
 	@Test
 	public void testQueryForObjec()
 	{
-		String sql = "SELECT username username, userpassword userpassword FROM t_user ";
+		String sql = "SELECT username username, userpassword userpassword FROM customers ";
 		RowMapper<User> rowMapper = new BeanPropertyRowMapper<>(User.class);
 		
 		User user = jdbcTemplate.queryForObject(sql, rowMapper, rowMapper);
@@ -64,7 +104,7 @@ public class JDBCTest {
 	@Test
 	public void testBatchUpdate()
 	{
-		String updateSQL = "INSERT INTO t_user(username,userpassword)  VALUES(?,?)";
+		String updateSQL = "INSERT INTO customers(username,userpassword)  VALUES(?,?)";
 		List<Object[]> batchArgs = new LinkedList<>();
 		batchArgs.add(new Object[]{"caoyanpei","cao"});
 		batchArgs.add(new Object[]{"wangxiaoyang","wang"});
@@ -77,7 +117,7 @@ public class JDBCTest {
 	@Test
 	public void testUpdate()
 	{
-		String updateSql = "insert into t_user values(?,?)";
+		String updateSql = "insert into customers values(?,?)";
 		jdbcTemplate.update(updateSql, "fanxueming", "fan");
 	}
 	
--- Spring-3/src/com/kang/spring/jdbc/User.java
+++ Spring-3/src/com/kang/spring/jdbc/User.java
@@ -4,29 +4,45 @@ public class User {
 
 	private String username;
 	
-	private String userpassword;
+	private String password;
+
+	private int balance;
+	
 
 	public String getUsername() {
 		return username;
 	}
+
 
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
 
-	public String getUserpassword() {
-		return userpassword;
+	public String getPassword() {
+		return password;
 	}
 
-	public void setUserpassword(String userpassword) {
-		this.userpassword = userpassword;
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
+
+
+	public int getBalance() {
+		return balance;
+	}
+
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
 
 	@Override
 	public String toString() {
-		return "User [username=" + username + ", password=" + userpassword + "]";
+		return "User [username=" + username + ", password=" + password + ", balance=" + balance + "]";
 	}
-	
+
 	
 }
