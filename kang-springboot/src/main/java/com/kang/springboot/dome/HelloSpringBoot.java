2aedbb3896b9568e002515e61a2f74a8d6302051
Lansefanggezi <lansefanggezi123@163.com>
2018-04-26 19:07:26 +0800

    Springboot

--- kang-springboot/src/main/java/com/kang/springboot/dome/HelloSpringBoot.java
+++ kang-springboot/src/main/java/com/kang/springboot/dome/HelloSpringBoot.java
@@ -2,12 +2,13 @@ package com.kang.springboot.dome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={RedisAutoConfiguration.class})
 @Controller
 @Configuration
 public class HelloSpringBoot {
@@ -20,6 +21,7 @@ public class HelloSpringBoot {
 	}
 	public static void main(String[] args)
 	{
-		SpringApplication .run(HelloSpringBoot.class, args);
+		SpringApplication application = new SpringApplication(HelloSpringBoot.class);
+		application.run(args);
 	}
 }
